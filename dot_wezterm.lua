45e41140ebe9bfd3caaf5d522b4d9137933142b5
Dmitry Tukeev <tukeev@gmail.com>
2026-01-14 14:23:46 +0300

    remove title bar for wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Window settings
+config.window_decorations = "RESIZE"
 config.initial_cols = 160
 config.initial_rows = 45
 
